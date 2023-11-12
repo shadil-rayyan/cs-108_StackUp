e5258389f29698c0e1b0ad0ddcb206226b96eb07
SHADIL AM <109820522+shadil-rayyan@users.noreply.github.com>
2023-11-13 00:16:26 +0530

    Update footer.jsx

--- resources/client/src/components/footer.jsx
+++ resources/client/src/components/footer.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import Logo from "../img/logo.png";
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <footer>
-      <img src={Logo} alt="" />
-      <span>
-        Made with ♥️ and <b>React.js</b>.
-      </span>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h3>Company</h3>
+          <ul>
+            <li><a href="/about-us">About Us</a></li>
+            <li><a href="/contact-us">Contact Us</a></li>
+            <li><a href="/privacy-policy">Privacy Policy</a></li>
+            <li><a href="/terms-and-conditions">Terms and Conditions</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Account</h3>
+          <ul>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/sign-up">Sign Up</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Social</h3>
+          <ul className="social-links">
+            <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+            <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            <li><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 };
