9e5cf1729ac872b1971859671f4a796725b30cb7
SHADIL AM <109820522+shadil-rayyan@users.noreply.github.com>
2023-11-15 01:06:01 +0530

    Add files via upload

---
+++ resources/api/routes/users.js
@@ -0,0 +1,15 @@
+import express from "express";
+import userController from "../controllers/user.js"; // Import the user controller
+
+const app = express();
+
+// Set up middleware, other routes, etc.
+
+// Use the user controller with the "/users" prefix
+app.use("/users", userController);
+
+// Start the Express app
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
